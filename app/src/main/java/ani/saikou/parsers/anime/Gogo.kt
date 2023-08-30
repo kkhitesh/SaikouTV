aa4f64e846d528c6a58d9b83125b5ad39aa05bd4
Hitesh Kowdiki <61322830+kkhitesh@users.noreply.github.com>
2023-08-30 12:04:33 +0530

    Update Gogo.kt

--- app/src/main/java/ani/saikou/parsers/anime/Gogo.kt
+++ app/src/main/java/ani/saikou/parsers/anime/Gogo.kt
@@ -52,18 +52,36 @@ class Gogo : AnimeParser() {
 
     override suspend fun getVideoExtractor(server: VideoServer): VideoExtractor? {
         val domain = Uri.parse(server.embed.url).host ?: return null
-        val extractor: VideoExtractor? = when {
-            "gogo" in domain    -> GogoCDN(server)
-            "goload" in domain  -> GogoCDN(server)
-            "playgo" in domain  -> GogoCDN(server)
-            "anihdplay" in domain  -> GogoCDN(server)
-            "taku" in domain  -> GogoCDN(server)
-            "sb" in domain      -> StreamSB(server)
-            "sss" in domain      -> StreamSB(server)
-            "fplayer" in domain -> FPlayer(server)
-            "fembed" in domain  -> FPlayer(server)
-            else                -> null
+        val extractor: VideoExtractor? = when (domain) {
+            "filemoon.to", "filemoon.sx"  -> FileMoon(server)
+            "rapid-cloud.co"              -> RapidCloud(server)
+            "streamtape.com"              -> StreamTape(server)
+            "vidstream.pro"               -> VidStreaming(server)
+            "mp4upload.com"               -> Mp4Upload(server)
+            "playtaku.net","goone.pro"    -> GogoCDN(server)
+            "alions.pro"                  -> ALions(server)
+            "awish.pro"                   -> AWish(server)
+            "dood.wf"                     -> DoodStream(server)
+            "ok.ru"                       -> OkRu(server)
+            "streamlare.com"              -> null // streamlare.com/e/vJ41zYN1aQblwA3g
+            else                          -> {
+                println("$name : No extractor found for: $domain | ${server.embed.url}")
+                null
+            }
         }
+        
+        // when {
+        //     "gogo" in domain    -> GogoCDN(server)
+        //     "goload" in domain  -> GogoCDN(server)
+        //     "playgo" in domain  -> GogoCDN(server)
+        //     "anihdplay" in domain  -> GogoCDN(server)
+        //     "taku" in domain  -> GogoCDN(server)
+        //     "sb" in domain      -> StreamSB(server)
+        //     "sss" in domain      -> StreamSB(server)
+        //     "fplayer" in domain -> FPlayer(server)
+        //     "fembed" in domain  -> FPlayer(server)
+        //     else                -> null
+        // }
         return extractor
     }
 
